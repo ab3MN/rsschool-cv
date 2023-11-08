d55b6900cd05cdb31fdb1bccced053517613e4e7
ab3mn3 <ab3mn3@gmail.com>
2023-11-08 12:19:10 +0200

    feat:add theme swticher

---
+++ js/index.js
@@ -0,0 +1,21 @@
+'use strict';
+
+const isDark = localStorage.getItem('isDarkTheme');
+const onDarkTheme = () => (
+  themeButton.classList.add('bx-sun'), root.classList.add('dark-theme')
+);
+isDark && onDarkTheme();
+
+const on = () => (onDarkTheme(), localStorage.setItem('isDarkTheme', true));
+const off = () => {
+  themeButton.classList.remove('bx-sun');
+  root.classList.remove('dark-theme');
+  localStorage.removeItem('isDarkTheme');
+};
+
+themeButton.onclick = () =>
+  localStorage.getItem('isDarkTheme') === null ? on() : off();
+
+navToggle.onclick = () => navMenu.classList.toggle('show-menu');
+
+navMenu.onclick = () => navMenu.classList.remove('show-menu');
